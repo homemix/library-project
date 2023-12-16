fbfb0fac462d4e35d8f9e2aa3ad239858b18e300
homemix <kenmutati@gmail.com>
2023-12-16 08:35:13 +0300

    add books create, read and tests.

--- books/tests.py
+++ books/tests.py
@@ -1,3 +1,27 @@
 from django.test import TestCase
 
-# Create your tests here.
+from django.urls import reverse
+from .models import Book
+
+
+class BooksTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.book = Book.objects.create(
+            title='Test Title',
+            subtitle='Test Subtitle',
+            author='Test Author',
+            isbn='Test ISBN',
+        )
+
+    def test_book_content(self):
+        self.assertEqual(self.book.title, 'Test Title')
+        self.assertEqual(self.book.subtitle, 'Test Subtitle')
+        self.assertEqual(self.book.isbn, 'Test ISBN')
+        self.assertEqual(self.book.author, 'Test Author')
+
+    def test_book_listview(self):
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed('books/book_list.html')
+        self.assertContains(response, 'Test Title')
